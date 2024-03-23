15813c8a5d8e6a29788149c7220a929f000cf67b
Oluchi Ochulo <ochulo02@byu.edu>
2024-03-22 21:15:11 -0600

    Add project files.

---
+++ Models/EFBookstoreRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mission11.Models
+{
+    public class EFBookstoreRepository : IBookStoreRepository
+    {
+        private BookstoreContext _context;
+        public EFBookstoreRepository(BookstoreContext temp) {
+            _context = temp;
+        }
+
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
---
+++ Models/IBookStoreRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+
+namespace Mission11.Models
+{
+    public interface IBookStoreRepository
+    {
+        public IQueryable<Book> Books { get; }
+    }
+}
