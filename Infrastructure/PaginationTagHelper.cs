aa4dbe47b4f07e8a9444a3c6ebfa3b7a7581cc4e
Oluchi Ochulo <ochulo02@byu.edu>
2024-03-22 22:50:15 -0600

    phase 2 and clean up

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -23,6 +23,10 @@ namespace Mission11.Infrastructure
         public string? PageAction {  get; set; }
         public PaginationInfo PageModel { get; set; }
 
+        public bool PageClassEnabled { get; set; } = false;
+        public string PageClass {  get; set; } = string.Empty;
+        public string PageClassNormal {  get; set; } = string.Empty;
+        public string PageClassSelected { get; set; } = string.Empty;
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null) 
@@ -36,6 +40,13 @@ namespace Mission11.Infrastructure
                     TagBuilder tag = new TagBuilder("a");
 
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+
+                    if (PageClassEnabled) 
+                    { 
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
+
                     tag.InnerHtml.Append(i.ToString());
 
                     result.InnerHtml.AppendHtml(tag);
